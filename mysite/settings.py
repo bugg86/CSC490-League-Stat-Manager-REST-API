6f2e39d0143717abe799a4d6694fd668594bc25d
bugg86 <jdfink@uncg.edu>
2022-02-14 20:34:06 -0800

    Add files via upload

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-8zw@5o=^vym(+ho0b04bb^irk7nl8_ditpvko7!-7s9cr@-8c=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [‘*’]
+ALLOWED_HOSTS = ['csc490-lsm-rest-api.azurewebsites.net']
+# ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -123,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
