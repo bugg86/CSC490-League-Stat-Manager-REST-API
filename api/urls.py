b8bdab1d832f9d31891425c51c4fff002b9694b4
bugg86 <jdfink@uncg.edu>
2022-02-14 20:34:41 -0800

    Add files via upload

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import include, path
 from rest_framework import routers
-from . import views
+
+from .views import ItemsViewset
 
 router = routers.DefaultRouter()
-router.register(r'items', views.ItemsViewset)
+router.register(r'items', ItemsViewset)
 
 urlpatterns = [
     path('', include(router.urls)),
--- api/views.py
+++ api/views.py
@@ -1,8 +1,14 @@
+from django.shortcuts import render
+from django.http.response import JsonResponse
+
 from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.parsers import JSONParser
+from rest_framework import status
 
 from .serializers import ItemsSerializer
 from .models import Items
 
-class ItemsViewset(viewsets.ModelViewSet) :
+class ItemsViewset(viewsets.ModelViewSet):
     queryset = Items.objects.all().order_by('itemid')
     serializer_class = ItemsSerializer
