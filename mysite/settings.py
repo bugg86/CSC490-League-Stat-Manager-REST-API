0b5ac459c74daac25bc64abc5e8e80dfebc40200
bugg86 <jdfink@uncg.edu>
2022-03-03 01:50:29 -0500

    I'm losing my mind

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8zw@5o=^vym(+ho0b04bb^irk7nl8_ditpvko7!-7s9cr@-8c='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-# ALLOWED_HOSTS = ['csc490-lsm-rest-api.azurewebsites.net', 'https://csc490-lsm-rest-api.net', 'https://csc490-lsm-rest-api.azurewebsites.net', 'csc490-lsm-rest-api.net']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['csc490-lsm-rest-api.azurewebsites.net', 'https://csc490-lsm-rest-api.net', 'https://csc490-lsm-rest-api.azurewebsites.net', 'csc490-lsm-rest-api.net']
+# ALLOWED_HOSTS = []
 
 
 # Application definition
